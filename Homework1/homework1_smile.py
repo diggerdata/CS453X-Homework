3612fb1e274ba9774f38f556b4ceb142e65b49d8
diggerdata <robotincad@gmail.com>
2019-03-17 13:01:02 -0400

    Began smile

--- Homework1/homework1_smile.py
+++ Homework1/homework1_smile.py
@@ -1,35 +1,43 @@
+#%%
 import numpy as np
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 
+#%%
 def fPC (y, yhat):
-    pass
-
+    return np.count_nonzero(y != yhat) / y.size()
+#%%
 def measureAccuracyOfPredictors (predictors, X, y):
     pass
 
+#%%
 def stepwiseRegression (trainingFaces, trainingLabels, testingFaces, testingLabels):
-    show = False
+    c1 = 0
+    c2 = 2
+    r1 = 0
+    r2 = 2
+    show = True
     if show:
         # Show an arbitrary test image in grayscale
         im = testingFaces[0,:,:]
         fig,ax = plt.subplots(1)
         ax.imshow(im, cmap='gray')
         # Show r1,c1
-        rect = patches.Rectangle((c1,r1),1,1,linewidth=2,edgecolor='r',facecolor='none')
+        rect = patches.Rectangle((c1-0.5,r1-0.5),1,1,linewidth=2,edgecolor='r',facecolor='none')
         ax.add_patch(rect)
         # Show r2,c2
-        rect = patches.Rectangle((c2,r2),1,1,linewidth=2,edgecolor='b',facecolor='none')
+        rect = patches.Rectangle((c2-0.5,r2-0.5),1,1,linewidth=2,edgecolor='b',facecolor='none')
         ax.add_patch(rect)
         # Display the merged result
         plt.show()
-
+#%%
 def loadData (which):
-    faces = np.load("{}ingFaces.npy".format(which))
+    faces = np.load("Homework1/{}ingFaces.npy".format(which))
     faces = faces.reshape(-1, 24, 24)  # Reshape from 576 to 24x24
-    labels = np.load("{}ingLabels.npy".format(which))
+    labels = np.load("Homework1/{}ingLabels.npy".format(which))
     return faces, labels
-
+#%%
 if __name__ == "__main__":
     testingFaces, testingLabels = loadData("test")
     trainingFaces, trainingLabels = loadData("train")
+    stepwiseRegression(trainingFaces, trainingLabels, testingFaces, testingLabels)
