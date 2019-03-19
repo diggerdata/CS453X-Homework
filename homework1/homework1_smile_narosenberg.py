caaa2eb653ce8652e1fe3e310a437f6f9b72a0eb
diggerdata <robotincad@gmail.com>
2019-03-19 15:51:24 -0400

    Formatting updates

--- homework1/homework1_smile_narosenberg.py
+++ homework1/homework1_smile_narosenberg.py
@@ -1,13 +1,11 @@
 #%%
+import copy
 import itertools
-from multiprocessing import Pool
-from os import cpu_count
 
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
-import copy
 
 #%%
 def fPC (y, yhat):
@@ -33,44 +31,49 @@ def process_combinations(face):
     return thresh
 #%%
 def stepwiseRegression (trainingFaces, trainingLabels, testingFaces, testingLabels, num_predictors=5):
+    i_permutation = permutate(trainingFaces[0].flatten())[:,0:2].T
+    predict = np.array([np.unravel_index(a, (24,24)) for a in i_permutation[0,:].astype(int)])
+    predict = np.append(predict, np.array([np.unravel_index(a, (24,24)) for a in i_permutation[1,:].astype(int)]), axis=1)
     results = "n,trainingAccuracy,testingAccuracy\r\n"
     # loop through each amount of training data
-    for j in range(400, 2400, 400):
-        i_permutation = permutate(trainingFaces[0].flatten())[:,0:2].T
-        predict = np.array([np.unravel_index(a, (24,24)) for a in i_permutation[0,:].astype(int)])
-        predict = np.append(predict, np.array([np.unravel_index(a, (24,24)) for a in i_permutation[1,:].astype(int)]), axis=1)
-        predictors = []
-        for k in range(num_predictors):
-            best_score = 0.0
-            best_predictor = None
-            for p in tqdm(predict):
-                test_predictors = copy.copy(predictors)
-                test_predictors.append(p.tolist())
-                accuracy = measureAccuracyOfPredictors(np.array(test_predictors), trainingFaces[:j], trainingLabels[:j])
-                if accuracy > best_score and p.tolist() not in predictors:
-                    best_predictor = p.tolist()
-                    best_score = accuracy
-            predictors.append(best_predictor)
-        predictors = np.array(predictors)
-        results = results + (str(j) + "," + str(measureAccuracyOfPredictors(predictors, trainingFaces[:j], trainingLabels[:j])) + "," + str(measureAccuracyOfPredictors(predictors, testingFaces, testingLabels)) + "\r\n")
+    predictors = []
+    with tqdm(total=5*num_predictors*len(predict), desc='Training...') as pbar:
+        for j in range(400, 2400, 400):
+            predictors = []
+            for k in range(num_predictors):
+                best_score = 0.0
+                best_predictor = None
+                for p in predict:
+                    test_predictors = copy.copy(predictors)
+                    test_predictors.append(p.tolist())
+                    accuracy = measureAccuracyOfPredictors(np.array(test_predictors), trainingFaces[:j], trainingLabels[:j])
+                    if accuracy > best_score and p.tolist() not in predictors:
+                        best_predictor = p.tolist()
+                        best_score = accuracy
+                    pbar.update(1)
+                predictors.append(best_predictor)
+            predictors = np.array(predictors)
+            results = results + (str(j) + "," \
+                + str(measureAccuracyOfPredictors(predictors, trainingFaces[:j], trainingLabels[:j])) \
+                + "," + str(measureAccuracyOfPredictors(predictors, testingFaces, testingLabels)) + "\r\n")
+    print("------------------------------")
+    print(results)
 
-        print(results)
-
-        show = True
-        if show:
-            # Show an arbitrary test image in grayscale
-            im = testingFaces[1,:,:]
-            fig,ax = plt.subplots(1)
-            ax.imshow(im, cmap='gray')
-            # Show r1,c1 for all predictors
-            for i in range(np.shape(predictors)[0]):
-                rect = patches.Rectangle((predictors[i,0]-0.5,predictors[i,1]-0.5),1,1,linewidth=2,edgecolor='r',facecolor='none')
-                ax.add_patch(rect)
-                # Show r2,c2
-                rect = patches.Rectangle((predictors[i,2]-0.5,predictors[i,3]-0.5),1,1,linewidth=2,edgecolor='b',facecolor='none')
-                ax.add_patch(rect)
-            # Display the merged result
-            plt.show()
+    show = True
+    if show:
+        # Show an arbitrary test image in grayscale
+        im = testingFaces[1,:,:]
+        fig,ax = plt.subplots(1)
+        ax.imshow(im, cmap='gray')
+        # Show r1,c1 for all predictors
+        for i in range(np.shape(predictors)[0]):
+            rect = patches.Rectangle((predictors[i,0]-0.5,predictors[i,1]-0.5),1,1,linewidth=2,edgecolor='r',facecolor='none')
+            ax.add_patch(rect)
+            # Show r2,c2
+            rect = patches.Rectangle((predictors[i,2]-0.5,predictors[i,3]-0.5),1,1,linewidth=2,edgecolor='b',facecolor='none')
+            ax.add_patch(rect)
+        # Display the merged result
+        plt.show()
 
 #%%
 def loadData (which):
