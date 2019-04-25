10053a8755b38440087586f822c5a168ff66830a
Nathan Rosenberg <robotincad@gmail.com>
2019-04-25 18:54:26 -0400

    Neural net done

--- homework6/test.py
+++ homework6/test.py
@@ -2,10 +2,11 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import scipy.optimize
+from copy import deepcopy
 from tqdm import tqdm, trange
 
 NUM_INPUT = 784  # Number of input neurons
-NUM_HIDDEN = 50  # Number of hidden neurons
+NUM_HIDDEN = 40  # Number of hidden neurons
 NUM_OUTPUT = 10  # Number of output neurons
 NUM_CHECK = 5  # Number of examples on which to check the gradient
 
@@ -13,22 +14,22 @@ NUM_CHECK = 5  # Number of examples on which to check the gradient
 # and return the individual weights and biases W1, b1, W2, b2.
 # This is useful for performing a gradient check with check_grad.
 def unpack (w):
-    W1 = w[0]
-    b1 = w[1]
-    W2 = w[2]
-    b2 = w[3]
+    W1 = w['W1']
+    b1 = w['b1']
+    W2 = w['W2']
+    b2 = w['b2']
     return W1, b1, W2, b2
 
 # Given individual weights and biases W1, b1, W2, b2, concatenate them and
 # return a vector w containing all of them.
 # This is useful for performing a gradient check with check_grad.
 def pack (W1, b1, W2, b2):
-    return [W1, b1, W2, b2]
+    return {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
 
 # Load the images and labels from a specified dataset (train or test).
 def loadData (which):
-    images = np.load("small_mnist_{}_images.npy".format(which))
-    labels = np.load("small_mnist_{}_labels.npy".format(which))
+    images = np.load("small_mnist_{}_images.npy".format(which)).T
+    labels = np.load("small_mnist_{}_labels.npy".format(which)).T
     return images, labels
 
 def plotSGDPath (trainX, trainY, ws):
@@ -56,99 +57,105 @@ def plotSGDPath (trainX, trainY, ws):
 
     plt.show()
 
-def ReLU(x):
-    return x * (x > 0)
-
-def dReLU(x):
-    return 1.0 * (x > 0)
-
-def softmax(x):
-    e = np.exp(x - np.max(x))  # prevent overflow
-    if e.ndim == 1:
-        return e / np.sum(e, axis=0)
-    else:  
-        return e / np.array([np.sum(e, axis=1)]).T  # ndim = 2
-
-def forward(x, w):
-    W1, b1, W2, b2 = unpack(w)
-    Z1 = np.dot(x, W1) + b1
-    A1 = ReLU(Z1)
-    Z2 = np.dot(A1, W2) + b2
-    A2 = softmax(Z2)
-    cache = pack(Z1, A1, Z2, A2)
-    return cache
-
-def backward(X, y, w, cache):
-    W1, _, W2, _ = unpack(w)
-    Z1, A1, Z2, A2 = unpack(cache)
-    g = np.multiply((A2 - y).T * W2, dReLU(Z1.T))
-    dW2 = (A2 - y) * ReLU(Z1).T
-    db2 = A2 -y
-    dW1 = g * X.T
-    db1 = g
-    return pack(dW1, db1, dW2, db2)
-
 # Given training images X, associated labels Y, and a vector of combined weights
 # and bias terms w, compute and return the cross-entropy (CE) loss. You might
 # want to extend this function to return multiple arguments (in which case you
 # will also need to modify slightly the gradient check code below).
-def fCE (X, y, w):
-    p = forward(X, w)[3]
-    m = y.shape[0]*2
-    return -np.sum(np.sum(y * np.log(p), axis=1), axis=0) / m
+def fCE (X, Y, w):
+    W1, b1, W2, b2 = unpack(w)
+    m = Y.shape[1] # number of example
+
+    # Compute the cross-entropy cost
+    logprobs = np.multiply(np.log(X), Y) + np.multiply((1 - Y), np.log(1 - X))
+    cost = - np.sum(logprobs) / m    
+    
+    cost = np.squeeze(cost)     
+    return cost
 
 # Given training images X, associated labels Y, and a vector of combined weights
 # and bias terms w, compute and return the gradient of fCE. You might
 # want to extend this function to return multiple arguments (in which case you
 # will also need to modify slightly the gradient check code below).
-def gradCE (X, y, w, lambd=0.7):
-    W1, _, W2, _ = unpack(w)
-    m = X.shape[0]
-    Z1, A1, Z2, A2 = unpack(forward(X, w))
-    g = (np.multiply(np.dot(W2, (A2 - y).T), dReLU(Z1.T))).T
-    dW2 = (1/m) * np.dot(ReLU(Z1).T, (A2 - y)) + (lambd/m)*W2
-    db2 = np.mean(A2 - y, axis=0)
-    dW1 = (1/m) * np.dot(X.T, g) + (lambd/m)*W1
-    db1 = np.mean(g, axis=0)
-    return pack(dW1, db1, dW2, db2)
-
-def updateWeights(w, grads, learning_rate=1e-4):
+def gradCE (X, Y, w):
     W1, b1, W2, b2 = unpack(w)
     dW1, db1, dW2, db2 = unpack(grads)
-    W1 += -learning_rate * dW1
-    b1 += -learning_rate * db1
-    W2 += -learning_rate * dW2
-    b2 += -learning_rate * db2
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
     return pack(W1, b1, W2, b2)
+
+def sigmoid(X):
+    return 1/(1+np.exp(-X))
+
+def softmax(x):
+    return np.exp(x) / np.exp(x).sum()
+
+def relu(x, derivative=False):
+    if derivative:
+        return 1*(x > 0)
+    else:
+        return x*(x > 0)
+
+def relu_backward(dA, Z):
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    return dZ
+
+def forwardProp(X, w):
+    W1, _, W2, _ = unpack(w)
+    h = x * W1
+    h = relu(h)
+    prob = softmax(h * W2)
+    return h, prob
+
+def backProp(X, y, w, cache):
+    m = X.shape[1]
+    W1, _, W2, _ = unpack(w)
+    _, A1, _, A2 = unpack(cache)
+    dZ2 = A2 - y
+    dW2 = (1 / m) * np.dot(dZ2, A1.T)
+    db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+    dW1 = (1 / m) * np.dot(dZ1, X.T)
+    db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+    return pack(dW1, db1, dW2, db2)
 
 def next_batch(X, y, batch_size):
     """Get a batch of `batch_size` from training data."""
     for i in np.arange(0, X.shape[0], batch_size):
         yield (X[i:i + batch_size], y[i:i + batch_size])
 
-def predict(X, y, w):
-    """Get predictions from a dataset and weigts. The lables (y) need to be in 
-    one-hot form.
-    """
-    A2 = forward(X, w)[3]
-    return A2 > 0.5
-
-def fPC(X, y, w):
-    """Compute the fPC."""
-    y_hat = predict(X, y, w)
-    return np.mean(y_hat == y)
+def updateWeights(w, grads, learning_rate=0.01):
+    W1, b1, W2, b2 = unpack(w)
+    dW1, db1, dW2, db2 = unpack(grads)
+    for batch_x, batch_y in next_batch(X, y, batch_size):
+            loss = fCE(batch_x, batch_y, w)
+            gradient = gradCE(batch_x, batch_y, w)
+            epoch_loss.append(loss)
+            w += -alpha * gradient
+    return   
 
 # Given training and testing datasets and an initial set of weights/biases b,
 # train the NN. Then return the sequence of w's obtained during SGD.
-def train (trainX, trainY, testX, testY, w, epochs=300, batch_size=256, learning_rate=0.1, print_loss=True):
-    ws = np.copy(w)
-    for epoch in trange(epochs):
-        for batch_x, batch_y in next_batch(trainX, trainY, batch_size): 
-            grads = gradCE(batch_x, batch_y, ws)
-            ws = updateWeights(ws, grads, learning_rate=learning_rate)
-        if print_loss and epoch % 10:
-            tqdm.write("Loss: {}".format(fCE(testX, testY, ws)))
-    print(fPC(testX, testY, ws))
+def train (trainX, trainY, testX, testY, w, epochs=500, print_cost=True, lr=0.01):
+    ws = deepcopy(w)
+    for i in trange(epochs):
+        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+        A2, cache = forwardProp(trainX, ws)
+        
+        # Cost function. Inputs: "A2, Y, parameters". Outputs: "cost".
+        cost = fCE(A2, trainY, ws)
+ 
+        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
+        grads = backProp(trainX, trainY, ws, cache)
+ 
+        # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
+        ws = updateWeights(ws, grads, lr=lr)
+        
+        # Print the cost every 1000 iterations
+        if print_cost and i % 10 == 0:
+            tqdm.write("Cost after iteration %i: %f" %(i, cost))
     return ws
 
 if __name__ == "__main__":
@@ -158,21 +165,20 @@ if __name__ == "__main__":
         testX, testY = loadData("test")
 
     # Initialize weights randomly
-    W1 = 2*(np.random.random(size=(NUM_INPUT, NUM_HIDDEN))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5
-    b1 = 0.01 * np.ones(NUM_HIDDEN)
-    W2 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_OUTPUT))/NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5
-    b2 = 0.01 * np.ones(NUM_OUTPUT)
+    W1 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_INPUT))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5
+    b1 = 0.01 * np.ones((NUM_HIDDEN,1))
+    W2 = 2*(np.random.random(size=(NUM_OUTPUT, NUM_HIDDEN))/NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5
+    b2 = 0.01 * np.ones((NUM_OUTPUT,1))
     w = pack(W1, b1, W2, b2)
 
     # Check that the gradient is correct on just a few examples (randomly drawn).
     idxs = np.random.permutation(trainX.shape[0])[0:NUM_CHECK]
-    print(gradCE(np.atleast_2d(trainX[idxs,:]), np.atleast_2d(trainY[idxs,:]), w)[0].shape)
     # print(scipy.optimize.check_grad(lambda w_: fCE(np.atleast_2d(trainX[idxs,:]), np.atleast_2d(trainY[idxs,:]), w_), \
     #                                 lambda w_: gradCE(np.atleast_2d(trainX[idxs,:]), np.atleast_2d(trainY[idxs,:]), w_), \
     #                                 w))
 
     # Train the network and obtain the sequence of w's obtained using SGD.
-    ws = train(trainX, trainY, testX, testY, w)
+    ws = train(trainX, trainY, testX, testY, w, lr=0.05)
 
     # Plot the SGD trajectory
     plotSGDPath(trainX, trainY, ws)
